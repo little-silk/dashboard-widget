13ff95b5efcc504fdf5151650add4b2ce38aafa7
little-silk <gabbyalvarez@comcast.net>
2022-05-02 19:41:49 -0700

    Dynamic background image

--- js/weather.js
+++ js/weather.js
@@ -12,7 +12,6 @@ const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${A
 fetch(url)
 .then( response => response.json() )
 .then( data => {
-
   // Check-check: Is data good? 
   console.log( data );
   
@@ -20,37 +19,19 @@ fetch(url)
   const weatherContainer = document.querySelector('.weather');
   const weatherType = (data.weather[0].main);
   console.log(weatherType);
+
+  document.body.style.backgroundImage = `url('assets/${weatherType}.jpeg')`;
+
+  console.log(document.body.style.backgroundImage);
+
   // Template to output
   const template = `
     <h1>Weather</h1>
     <data value="${data.name}" class="city">${data.name}</data>
     <data value="${data.main.temp}" class="temp">${data.main.temp}&#8457;</data>
     <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="Placeholder">
-    <img src=assets/${weatherType}>
   `;
   
   // Insert dynamic template to container
   weatherContainer.insertAdjacentHTML("afterbegin", template);
-  
 });
-
-const cloudy = new Image();
-cloudy.src =  "assets/clouds.jpeg";
-
-const clear = new Image();
-clear.src = "assets/clear.jpeg";
-
-const drizzle = new Image();
-drizzle.src = "assets/drizzle";
-
-const rain = new Image();
-rain.src = "assets/rain";
-
-const snow = new Image();
-snow.src = "assets/snow";
-
-const thunderstorm = new Image();
-thunderstorm.src = "assets/thunderstorm";
-
-
-
